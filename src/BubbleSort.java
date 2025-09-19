b940aa4b6cd2c31e33fe25cf1ce15807e4651ec5
Vijay Krishna <vkk7739@gmail.com>
2025-09-20 01:57:07 +0530

    BubbleSort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -2,29 +2,33 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {3,4,1,2,0,7,0,9,3};
+        int[] arr = {1,2,3,4,1,2};
         bubble(arr);
         System.out.println(Arrays.toString(arr));
     }
     private static void bubble(int[] arr) {
         int n = arr.length;
         for (int pass = 0;pass < n-2;pass++){
+            boolean swaps = false;
         for (int i = 0; i < n-pass-1; i++) {
-        if (arr[i] > arr[i + 1]) {
+        if (arr[i] < arr[i + 1]) {
             swap(arr,i,i+1);
+            swaps = true;
         }
     }
+        if(!swaps) break;
 }
-
-
     }
     public static void swap(int[] arr,int i , int j){
         while(i<j) {
+
             int temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
             i++;
             j--;
+
         }
+
     }
 }
