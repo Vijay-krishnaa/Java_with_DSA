633874f9ac2717f5d01046c02e2dd33e82f85c97
Vijay Krishna <vkk7739@gmail.com>
2025-09-26 13:41:38 +0530

    maximumCount

---
+++ src/maximumCount.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class maximumCount {
+    public static int maximumCountt(int[] nums) {
+        int n = nums.length;
+        int firstPos = firstPositive(nums);
+        int lastNeg = lastNegative(nums);
+        int countPos = (firstPos == -1) ? 0 : (n - firstPos);
+        int countNeg = (lastNeg == -1) ? 0 : (lastNeg + 1);
+        return Math.max(countPos, countNeg);
+    }
+    private static int firstPositive(int[] nums) {
+        int s = 0, e = nums.length - 1;
+        int ans = -1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (nums[mid] > 0) {
+                ans = mid;
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return ans;
+    }
+    private static int lastNegative(int[] nums) {
+        int s = 0, e = nums.length - 1;
+        int ans = -1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (nums[mid] < 0) {
+                ans = mid;
+                s = mid + 1; 
+            } else {
+                e = mid - 1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] nums = {-3, -2, -1, 0, 0, 1, 2};
+        System.out.println(maximumCountt(nums));
+
+    }
+}
--- src/peakIndexInMountainArray.java
+++ src/peakIndexInMountainArray.java
@@ -3,10 +3,8 @@ class Solution {
         int n = arr.length;
         int s = 0;
         int e = n - 1;
-
         while (s < e) {
             int mid = s + (e - s) / 2;
-
             if (arr[mid] < arr[mid + 1]) {
                 s = mid + 1;
             } else {
@@ -15,10 +13,9 @@ class Solution {
         }
         return s;
     }
-
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int[] arr = {0, 2, 4, 6, 5, 3, 1}; // example mountain array
+        int[] arr = {0, 2, 4, 6, 5, 3, 1};
         int peakIndex = sol.peakIndexInMountainArray(arr);
         System.out.println("Peak Index: " + peakIndex);
         System.out.println("Peak Value: " + arr[peakIndex]);
